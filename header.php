c1b9f0462ddc7e7cf6a32258f1a8d3db33c276f6
animeshvashisth24 <84134397+animeshvashisth24@users.noreply.github.com>
2022-10-14 23:59:44 +0530

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-
+<!--  adding collapse class feature-->
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
