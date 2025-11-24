46c5f7a3c50540a7a458d4cf7884cea564802d84
animeshvashisth24 <84134397+animeshvashisth24@users.noreply.github.com>
2025-11-24 23:14:59 +0530

    Update about.php

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="css/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-  <!--  font family -->
+  <!-- font family -->
   <link href="https://fonts.googleapis.com/css2?family=Gideon+Roman&family=Titillium+Web&display=swap" rel="stylesheet">
 
 </head>
