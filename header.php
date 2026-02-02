d67fb729926200393cce35032ab3f2279be0da6a
animeshvashisth24 <84134397+animeshvashisth24@users.noreply.github.com>
2026-02-02 23:46:13 +0530

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<!--  navbar -->
+<!-- navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <a class="navbar-brand" href="#"><img id="logo" src="images/logo.png"></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
